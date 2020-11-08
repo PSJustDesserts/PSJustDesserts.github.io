0d0ac554cb90f4fa26429e8daf4eb88eb720034a
PSJustDesserts <74152050+PSJustDesserts@users.noreply.github.com>
2020-11-08 13:50:44 -0700

    Update FIles

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,3 @@
 $("document").ready(function() {
-  $.getJSON("../data.json", function(data) {
-    const siteData = data;
-    $("title").html(siteData.name);
-  }).fail(function() {
-    console.log("Err");
-  });
+  
 });
